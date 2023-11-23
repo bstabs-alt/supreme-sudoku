9d0fe4a642550235deddcdc9d3641f5d1e5af401
bstabs-alt <reubensweetmangough@gmail.com>
2023-11-23 23:58:13 +1300

    basic boilerplate

---
+++ SupremeSudokuCSharp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace SupremeSudokuCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int difficulty = Setup();
+            SudokuBoard board = new SudokuBoard(difficulty);
+
+            int[,] sudokuBoard = new int[9, 9]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+
+            board.PrintBoard(sudokuBoard);
+        }
+
+
+
+        private static int Setup()
+        {
+            Console.WriteLine("Supreme Sudoku!");
+            Console.WriteLine("1. Easy\n" + "2.Medium\n" + "3.Hard\n");
+            int difficulty = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Creating Sudoku Board: ");
+            switch (difficulty)
+            {
+                case 1:
+                    Console.WriteLine("Easy");
+                    return difficulty;            
+                case 2:
+                    Console.WriteLine("Medium");
+                    return difficulty;
+                case 3:
+                    Console.WriteLine("Hard");
+                    return difficulty; 
+                default:
+                    Console.WriteLine("Invalid");
+                    return 0;
+            }
+        }
+    }
+}
---
+++ SupremeSudokuCSharp/SudokuBoard.cs
@@ -0,0 +1,27 @@
+﻿namespace SupremeSudokuCSharp
+{
+    public class SudokuBoard
+    {
+        private int difficulty;
+
+        public SudokuBoard(int difficulty)
+        {
+            this.difficulty = difficulty;
+        }
+
+        public int Difficulty
+        {
+            get { return difficulty; }
+            set { difficulty = value; }
+        }
+
+        public void PrintBoard(int[,] board)
+        {
+            Console.WriteLine("Printing Board");
+            Console.WriteLine("-------------");
+
+          
+        }
+
+    }
+}
